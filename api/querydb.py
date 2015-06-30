12512381c4c7096e384952e5b4d492736a5c22d7
karottc <karottc@gmail.com>
2015-06-30 08:30:22 +0800

    增加查询数据库相关的class。

--- api/querydb.py
+++ api/querydb.py
@@ -1,1 +1,89 @@
-__author__ = 'k'
+#coding=UTF-8
+__author__ = 'karottc'
+
+import MySQLdb
+import json
+import logging
+import time
+
+__metaclass__=type
+
+class QueryDB:
+    def __init__(self):
+        self.db = MySQLdb.connect("localhost","root","","zhihu", use_unicode=True, charset="utf8")
+        self.cursor = self.db.cursor()
+        self.indexNum = 10
+        now = time.time()
+        year, mon, day, hour, minutes, val, val, val, val = time.localtime(now)
+        logPath = '/root/testproject/mysite/log/zhihu_%d%02d%02d.log' % (year, mon, day)
+        logPath = 'zhihu_%d%02d%02d.log' % (year, mon, day)
+        logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(funcName)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename=logPath,
+                filemode='a')
+
+    def __del__(self):
+        self.db.close()
+
+    def QueryIndex(self, timestamp=0):
+        if 0 == timestamp:
+            sql = 'select fstoryid,ftitlename,fnum, frecomm, ftitlepic, ftitlebigpic, fdisplaydate, fupdatetime from \
+                   t_zhihu_daily_index ORDER BY fupdatetime DESC LIMIT %d' % self.indexNum
+        else:
+            sql = 'select fstoryid,ftitlename,fnum, frecomm, ftitlepic, ftitlebigpic, fdisplaydate, fupdatetime from t_zhihu_daily_index \
+               WHERE fupdatetime<%d ORDER BY fupdatetime DESC LIMIT %d' % (timestamp, self.indexNum)
+
+        try:
+            self.cursor.execute(sql)
+            results = self.cursor.fetchall()
+            listRet = []
+
+            for row in results:
+                dictStory = {}
+                dictStory['id'] = row[0]
+                dictStory['images'] = row[4]
+                dictStory['title'] = row[1]
+                dictStory['display_date'] = row[6]
+                dictStory['timestamp'] = row[7]
+                listRet.append(dictStory)
+            logging.info(listRet)
+            return listRet
+        except Exception, e:
+            logging.error(e)
+            return []
+
+    def QueryStory(self, id):
+        sql = 'select fstoryid, ftitle, fnum, fanwser, fviewmore from t_zhihu_daily_story where fstoryid=%d ORDER BY fid' % id
+        try:
+            self.cursor.execute(sql)
+            results = self.cursor.fetchall()
+            listRet = []
+
+            for row in results:
+                dictStory = {}
+                dictStory['question_title'] = row[1]
+                dictStory['answer_num'] = row[2]
+                strTemp = row[3]
+                # 这个是为了处理{"answer": "<img src="http://pic4.zhimg.com/49c977e6c68beccfe8f594258b397067_b.jpg" 这种情况
+                # 这种会和json的属性双引号冲突
+                pos = strTemp.find('img src')
+                endPos = strTemp.find('>')
+                if pos != -1:
+                    str1 = strTemp[0:pos]
+                    str2 = strTemp[pos:endPos+1]
+                    str3 = strTemp[endPos+1:]
+                    str2 = str2.replace('"', "'")
+                    strTemp = str1 + str2 + str3
+                dictStory['answers'] = json.loads(strTemp, strict=False)
+                #dictStory['answer'] = row[3]
+                dictStory['viewmore'] = row[4]
+                listRet.append(dictStory)
+            logging.info(listRet)
+            return listRet
+        except Exception, e:
+            logging.error(e)
+            return []
+
+#gt = QueryDB()
+#print gt.QueryStory(4769094)
