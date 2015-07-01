937ccdac814062328acf8529cbf2da6b9f47ea08
karottc <karottc@gmail.com>
2015-07-02 00:40:12 +0800

    完成主要接口编码。 一共三个接口：首页接口，刷新接口，获取详情接口。

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^gethomelist$', 'api.views.GetHomeList'),
     url(r'^getnextlist$', 'api.views.GetNextList'),
+    url(r'^getstorydetail$', 'api.views.GetStoryDetail'),
 )
--- api/views.py
+++ api/views.py
@@ -1,27 +1,67 @@
 #coding=UTF-8
-from django.http import HttpResponse
-import simplejson
+__author__ = 'karottc'
 
+from django.http import HttpResponse
+import json
+import sys
+from querydb import *
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def GetHomeList(request):
+    qd = QueryDB()
     content = {}
 
-    content['number'] = 20
+    listStory = qd.QueryIndex()
+    content['number'] = len(listStory)
     content['ret'] = 0
     content['err'] = ""
-    result = simplejson.dumps(content, ensure_ascii=False)
+    content['stories'] = listStory
+    result = json.dumps(content, ensure_ascii=False)
+    logging.info(result)
     return HttpResponse(result, content_type='application/json; charset=utf-8')
 
 def GetNextList(request):
+    qd = QueryDB()
+
     content = {}
-    content['number'] = 20
     content['ret'] = 0
     content['err'] = ""
     try:
         id = int(request.GET['timestamp'])
+        listStory = qd.QueryIndex(id)
+        content['number'] = len(listStory)
+        content['err'] = ""
+        content['ret'] = 0
+        content['stories'] = listStory
     except Exception, e:
         content['number'] = 0
         content['err'] = str(e)
         content['ret'] = 1
-    result = simplejson.dumps(content, ensure_ascii=False)
+    result = json.dumps(content, ensure_ascii=False)
+    logging.info(result)
+    return HttpResponse(result, content_type='application/json; charset=utf-8')
+
+def GetStoryDetail(request):
+    qd = QueryDB()
+
+    content = {}
+
+    content['ret'] = 0
+    content['err'] = ""
+    try:
+        id = int(request.GET['id'])
+        listStory = qd.QueryStory(id)
+        content['question_num'] = len(listStory)
+        content['err'] = ""
+        content['ret'] = 0
+        content['stories'] = listStory
+    except Exception, e:
+        content['number'] = 0
+        content['err'] = str(e)
+        content['ret'] = 1
+        print e
+    result = json.dumps(content, ensure_ascii=False)
+    logging.info(result)
     return HttpResponse(result, content_type='application/json; charset=utf-8')
