83323f5d2f99e104427f692fa5a474db6ccc940c
karottc <karottc@gmail.com>
2015-07-03 01:35:26 +0800

    增加接口文档；关闭调试模式。

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n3#@h!#_08sop$8fsbyh*r#f2dv13wjj#2_6_yl_^c@@a%kqj&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
